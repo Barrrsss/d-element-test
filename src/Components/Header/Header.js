53801bba9f1b02b690edf500847be3b1d7d5bc0d
Aleksey Krasnov <neky@list.ru>
2021-06-08 22:21:01 +0500

    final

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import './Header.scss';
 import logo from '../../images/header-logo.svg'
+import {NavLink} from "react-router-dom";
+
 function Header(props) {
     return (
         <header className="header">
-            <img className="header__logo" src={logo} alt="логотип" />
+            <NavLink to="/">
+                <img className="header__logo" src={logo} alt="логотип" />
+            </NavLink>
             <button className="header__menu" type="button" onClick={props.handleMenuOpen} />
         </header>
     );
